3f0aa49ca0f3c9e8c4e4e30e781c4c65072f19af
Anwar Nadeem <ibnul.aftab@proton.me>
2025-03-31 17:53:46 +0500

    added a class

--- running.py
+++ running.py
@@ -1,8 +1,20 @@
 from psutil         import Process
+
 from win32gui       import GetForegroundWindow
 from win32process   import GetWindowThreadProcessId
+from time           import sleep
 
-def process ():
-    return Process(GetWindowThreadProcessId(GetForegroundWindow())[1])
 
-print (process ())
+class program:
+    def __init__ (self, application):
+        processDict = application.as_dict()
+        self.name = processDict["name"]
+        self.startTime = processDict["create_time"]
+        self.pid = processDict["pid"]
+
+def foregroundProcess ():
+    return Process (GetWindowThreadProcessId (GetForegroundWindow ()) [1])
+
+app = program (foregroundProcess ())
+
+print (app.pid)
