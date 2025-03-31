856492843d5175e71fb773f763e24ca4c969fd46
Anwar Nadeem <ibnul.aftab@proton.me>
2025-03-31 19:06:24 +0500

    implemeted is running function in the class

--- running.py
+++ running.py
@@ -5,8 +5,12 @@ from time           import sleep
 from datetime       import datetime
 
 class program:
+    __app = None
+
     def __init__ (self, application):
         processDict = application.as_dict()
+        
+        self.__app = application
         self.name = processDict["name"]
         self.startTime = processDict["create_time"]
         self.pid = processDict["pid"]
@@ -14,9 +18,12 @@ class program:
     def started (self):
         return datetime.fromtimestamp (self.startTime).strftime("%d/%m/%y: %I:%M:%S %p")
 
+    def isRunning (self):
+        return __app.is_running ()
+
 def foregroundProcess ():
     return Process (GetWindowThreadProcessId (GetForegroundWindow ()) [1])
 
 app = program (foregroundProcess ())
 
-print (app.started ())
+print (app.isRunning())
