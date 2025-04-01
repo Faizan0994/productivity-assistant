3cd309d1c78197f7aea9e2bfab4b751f1ec28ad8
Anwar Nadeem <ibnul.aftab@proton.me>
2025-04-01 16:33:44 +0500

    some edits? same functionality

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import process
 from time import sleep
+import process
 
 current = []
 while True:
-    app = process.program (process.current_process ())
-    isCurrent = process.is_current_process (app, current)
-    if isCurrent:
-        print ("I am here!")
-    else:
-        current.append (app)
-        for x in current:
-            print (x.name)
-        print ()
-
+    current.sort (key = lambda application: application.name)
+    app = process.program (process.foreground_process ())
+    
+    # checking if the function exists...
+    if app.name != None:
+        isCurrent = process.in_current (app, current)
+        if isCurrent:
+            # do something...
+            pass
+        else:
+            # do something... 
+            current.append (app)
+        
+    for x in current:
+        print (x.name, end = " ")
+    print()
     sleep (1)
