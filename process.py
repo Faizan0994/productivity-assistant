d2354438908722bb9906f10bab7d52f539828d63
Anwar Nadeem <ibnul.aftab@proton.me>
2025-04-01 16:14:33 +0500

    I worked for hours and now I am going to delete it? I failed to make binary search!

--- process.py
+++ process.py
@@ -1,10 +1,10 @@
-from psutil         import Process
+from psutil         import Process, NoSuchProcess, AccessDenied
 from win32gui       import GetForegroundWindow
 from win32process   import GetWindowThreadProcessId
 from datetime       import datetime
 
 # Program class represents the required information of the Process object
-# for  example, its name, date of creation and process ID. The stat  time
+# for  example, its name, date of creation and process ID. The strat time
 # is  stored in datetime object   and hence is of float type. This  *may* 
 # prove  uselful in storing and  processing data. Hence another  function 
 # was  made to display time in more readable fasion. The running  fnction
@@ -15,19 +15,39 @@ from datetime       import datetime
 # pid_exists  method for pids are  recycled. There is also a function  to 
 # see if two programs are same by seeing if their names are same.
 
+# During  intialization,  it is possible that None is passed due  to  some 
+# error. Hence, the type is compared to determine if its an Process object 
+# or NoneType. For encountering an error the name is set to none.
+
 class program:
     __app = None
 
     def __init__ (self, application):
-        self.__app = application
+        if isinstance (application, Process):
+            self.__app = application
         
-        with application.oneshot ():
-            self.name = application.name ()
-            self.startTime = application.create_time ()
-            self.pid = application.pid
-    
+            with application.oneshot ():
+                self.name = application.name ()
+                self.startTime = application.create_time ()
+                self.pid = application.pid
+         
+        elif isinstance (application, type (None)):
+            self.name = None
+
     def __eq__ (self, other):
         if self.name == other.name:
+            return True
+        else:
+            return False
+    
+    def __gt__ (self, other):
+        if self.name > other.name:
+            return True
+        else:
+            return False
+
+    def __lt__ (self, other):
+        if self.name < other.name:
             return True
         else:
             return False
@@ -43,8 +63,8 @@ class program:
 # window).  As  we need the PID, we pass it  to  GetWindowThreadProcessID 
 # which gives both the process and  thread id. GetProcessID can't be used
 # as  it accepts pyHWND which I tried and have asked but failed. The  pid 
-# is passed to Process to make a Process object. The Process can raise 
-# the two errors, on which it returns None.
+# is  passed  to Process to make a Process object. For  NoSuchErrors  and 
+# AccessDenied return is None. See program commment.
 
 def foreground_process ():
     try:
@@ -53,11 +73,39 @@ def foreground_process ():
         return None
     except AccessDenied:
         return None
+    except ValueError:
+        return None
 
 # Was thinking of implementing binary search... 
 
-def is_current_process (app, appList):
-    for currentApp in appList:
-        if currentApp == app:
-            return True
-    return False
+def in_current (app, orderedAppList):
+    length = len (orderedAppList)
+    
+    if length == 0:
+        return False
+    elif length == 1:
+            if orderedAppList[0] == app:
+                return True
+            else:
+                return False
+    else:
+        if length % 2 == 0:
+            index = int(length / 2)
+            
+            if orderedAppList[index] == app or orderedAppList[index - 1] == app:
+                return True
+            else:
+                if orderedAppList[index] < app:
+                    in_current (app, orderedAppList[index + 1: length - 1])
+                else:
+                    in_current (app, orderedAppList[0: index - 2])
+        else:
+            index = int(length // 2)
+            
+            if orderedAppList[index] == app:
+                return True
+            else:
+                if orderedAppList[index] < app:
+                    in_current (app, orderedAppList[index + 1: length - 1])
+                else:
+                    in_current (app, orderedAppList[0:index - 1])
