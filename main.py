931feffd9017a54f428b5b9be1fc35b0e627bd3d
Anwar Nadeem <ibnul.aftab@proton.me>
2025-04-02 12:18:33 +0500

    funtion-ify the main

--- main.py
+++ main.py
@@ -2,27 +2,30 @@ from time import sleep
 
 import process
 
-# main loop flag
+# important control flags
 asstiantRunning = True
 
 current = []
 
-while True:
-    # if not needed remove it
-    current.sort (key = lambda application: application.name)
-    
-    app = process.program (process.foreground_process ())
-    
-    # checking if the function exists...
-    if app.name != None:
-        if process.in_current (app, current):
-            # do something...
-            pass
-        else:
-            current.append (app)
-            # do something... 
-        
-    for x in current:
-        print (x.name, end = " ")
-    print()
-    sleep (1)
+def assistant (currentAppList):
+    while True:
+	    # if not needed remove it
+	  	currentAppList.sort (key = lambda application: application.name)
+	  	
+	  	app = process.program (process.foreground_process ())
+	  	
+	  	# checking if the function exists
+	  	if app.name != None:
+	  	    if process.in_current (app, current):
+	  	        # do something...
+	  	        print (app.start_time ())
+	  	        pass
+	  	    else:
+	  	        currentAppList.append (app)
+	  	        # do something... 
+	  	    
+	  	for application in currentAppList:
+	  	    print (application.name, end = " ")
+	  	print()
+	  	
+        sleep (1)
