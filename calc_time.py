3fd4a0e41b6844ebb0acbfbdd16e4de20b300e6d
Anwar Nadeem <ibnul.aftab@proton.me>
2025-04-05 17:49:42 +0500

    implemented function for conversting local time to utc

--- calc_time.py
+++ calc_time.py
@@ -1,6 +1,10 @@
 from tzlocal    import get_localzone_name
 from pytz       import timezone
+from datetime   import datetime
 
 def add_local_tz (datetimeObj):
     zone = timezone (get_localzone_name ())
     return zone.localize (datetimeObj)
+
+def to_utc (datetimeObj):
+    return datetimeObj.astimezone (timezone ("UTC"))
