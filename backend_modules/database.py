5ac0681038e5d5097b39d74403bb8253508d481f
Anwar Nadeem <ibnul.aftab@proton.me>
2025-04-10 08:59:10 +0500

    created var
    this var contains the path to database

--- backend_modules/database.py
+++ backend_modules/database.py
@@ -1,5 +1,15 @@
 from win32api   import GetUserName
 from pathlib    import PureWindowsPath
+from os         import access, listdir, mkdir
+from os.path    import expanduser, join, isdir
+import os
 
+# global variables
 userName = GetUserName()
-databasePath = PureWindowsPath ("c:/", "User", userName, "AppData")
+
+databasePath = join (expanduser ("~"), PureWindowsPath ("AppData", "Local", "Productivity Assistant"))
+freshDownload = not isdir (databasePath)
+if freshDownload:
+        mkdir (databasePath)
+        # run the introductory program...
+del (freshDownload)
