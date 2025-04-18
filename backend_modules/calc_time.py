f485fc092e02fc296f79fe418706c96eed56fcd8
Anwar Nadeem <ibnul.aftab@proton.me>
2025-04-18 19:57:57 +0500

    added function that returns the weeks

--- backend_modules/calc_time.py
+++ backend_modules/calc_time.py
@@ -1,13 +1,21 @@
 from tzlocal    import get_localzone_name
 from pytz       import timezone
 from datetime   import datetime
+from datetime   import timedelta
 
 def current_time ():
     return add_local_tz(datetime.today ())
 
-def add_local_tz (datetimeObj):
+def add_local_tz (datetimeObj: datetime):
     zone = timezone (get_localzone_name ())
     return zone.localize (datetimeObj)
 
-def to_utc (datetimeObj):
+def to_utc (datetimeObj: datetime):
     return datetimeObj.astimezone (timezone ("UTC"))
+
+def weekdays () -> list:
+    weeklist = []
+    today = current_time ()
+    for n in range (7):
+        weeklist.append ((today - timedelta (days = n)).weekday ())
+    return weeklist
