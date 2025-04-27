dc6d693e15b8318974c53929c05b1609e10b4069
Anwar Nadeem <ibnul.aftab@proton.me>
2025-04-27 18:23:16 +0500

    breaking your code :)

--- gui_data.py
+++ gui_data.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
-from backend_modules.database import most_used_app, time_spent
-from backend_modules.calc_time import current_time, to_utc, convert_seconds
+from backend_modules.database import most_used_app, time_spent, cordinates
+from backend_modules.calc_time import current_time, to_utc, convert_seconds,  weekdays, x_points
 
 midnight = to_utc(datetime.combine(datetime.today(), datetime.min.time())).isoformat()
 lastMidnight = to_utc(datetime.combine(datetime.today() - timedelta(days=1), datetime.min.time())).isoformat()
@@ -8,7 +8,7 @@ currentTime = to_utc(current_time()).isoformat()
 startOfWeek = to_utc(current_time() - timedelta(weeks=1)).isoformat()
 startOfLastWeek = to_utc(current_time() - timedelta(weeks=2)).isoformat()
 
-mostUsedApp = most_used_app(midnight, currentTime)[0] # first element is the name of the app
+# mostUsedApp = most_used_app(midnight, currentTime)[0] # first element is the name of the app
 # calculate screen time
 timeSpentSeconds = int(time_spent(midnight, currentTime).total_seconds())
 timeSpent = convert_seconds(timeSpentSeconds)
@@ -44,3 +44,9 @@ else:
 # Readable string format of time spent
 screenTimeToday = f"{timeSpent[0]}h {timeSpent[1]}m {timeSpent[2]}s"
 screenTimeThisWeek = f"{weeklyTimeSpent[0]}h {weeklyTimeSpent[1]}m {weeklyTimeSpent[2]}s"
+
+now = current_time ()
+xPoints = x_points (now - timedelta (weeks = 1), now)
+xPoints = [weekdays (i[0]) for i in cordinates (xPoints)]
+
+print (xPoints)
