d388d2e5817f907d766a75a8aca1bc4e80d2b6af
Faizan <faizanamjad575@gmail.com>
2025-04-29 10:49:31 +0500

    remove utc conversions

--- gui_data.py
+++ gui_data.py
@@ -2,11 +2,11 @@ from datetime import datetime, timedelta
 from backend_modules.database import most_used_app, time_spent, cordinates
 from backend_modules.calc_time import current_time, to_utc, convert_seconds, weekdays, x_points, total_hours
 
-midnight = to_utc(datetime.combine(datetime.today(), datetime.min.time())).isoformat()
-lastMidnight = to_utc(datetime.combine(datetime.today() - timedelta(days=1), datetime.min.time())).isoformat()
-currentTime = to_utc(current_time()).isoformat()
-startOfWeek = to_utc(current_time() - timedelta(weeks=1)).isoformat()
-startOfLastWeek = to_utc(current_time() - timedelta(weeks=2)).isoformat()
+midnight = datetime.combine(datetime.today(), datetime.min.time()).isoformat()
+lastMidnight = datetime.combine(datetime.today() - timedelta(days=1), datetime.min.time()).isoformat()
+currentTime = current_time().isoformat()
+startOfWeek = (current_time() - timedelta(weeks=1)).isoformat()
+startOfLastWeek = (current_time() - timedelta(weeks=2)).isoformat()
 
 mostUsedApp = most_used_app(midnight, currentTime) [0] # first element is the name of the app
 # calculate screen time
