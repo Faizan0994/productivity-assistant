7670900c440c554bcb2c7a3da458610430eae90a
Faizan <faizanamjad575@gmail.com>
2025-05-12 07:34:34 +0500

    create functionality for launching at startup and running in background

--- background.py
+++ background.py
@@ -1,14 +1,53 @@
 from threading  import Thread
+import os
+import winreg
+import sys
 from psutil     import process_iter
 from main       import assistant
 from gui        import main             as gui
 
 background = Thread (target = assistant)
 
+# Using registery editing
+def add_to_startup(app_name, exe_path=None):
+    if exe_path is None:
+        exe_path = sys.executable # executable's path
+
+    key = winreg.OpenKey(
+        winreg.HKEY_CURRENT_USER,
+        r"Software\Microsoft\Windows\CurrentVersion\Run",
+        0, winreg.KEY_SET_VALUE
+    )
+    winreg.SetValueEx(key, app_name, 0, winreg.REG_SZ, f'"{exe_path}" --silent') # launch in silent mode at startup
+    winreg.CloseKey(key)
+
+
+def is_first_run(flag_file="first_run.flag"):
+    return not os.path.exists(flag_file)
+
+
+def mark_first_run_done(flag_file="first_run.flag"):
+    try:
+        with open(flag_file, "w") as f:
+            f.write("done")
+    except:
+        pass
+
+if is_first_run():
+    add_to_startup("productivity_assistant")
+    mark_first_run_done()
+
+
+def is_silent_mode():
+    return "--silent" in sys.argv
+
+
+
 for process in process_iter (['name']):
     if not process == __name__:
         background.start ()
-        gui ()
-        background.join ()
+        if not is_silent_mode():
+            gui ()
+        background.join()
     else:
         gui ()
